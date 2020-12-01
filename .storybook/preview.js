99f2304aca8f09392d7e22fe0771a3ff42d26d3f
Sam Huynh <samhwang2112.dev@gmail.com>
2020-12-02 09:47:56 +1100

    Update ESLint, Babel, Jest and ts config for React 17

---
+++ .storybook/preview.js
@@ -0,0 +1,23 @@
+import { configure } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+
+configure(require.context('../src', true, /\.stories\.(jsx?|tsx?)$/), module);
+
+global.___loader = {
+  enqueue: () => {},
+  hovering: () => {},
+};
+
+global.__PATH_PREFIX__ = '';
+
+window.___navigate = (pathname) => {
+  action('NavigateTo:')(pathname);
+};
+
+/**
+ * Temporary fix
+ * @see: https://github.com/gatsbyjs/gatsby/issues/28253
+ */
+config.resolve.alias[
+  "$virtual/lazy-client-sync-requires"
+] = require.resolve('../src/__mocks__/virtual-lazy-client-sync-requires')
---
+++ src/__mocks__/virtual-lazy-client-sync-requires.js
@@ -0,0 +1,2 @@
+exports.notVisitedPageComponents = [];
+exports.components = [];
