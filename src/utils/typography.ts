466316709443d75e3292593c1f9069856d9d910a
Sam Huynh <samhwang2112.dev@gmail.com>
2020-12-02 09:17:52 +1100

    Amend Typography component separately without overriding the actual import

--- src/utils/typography.ts
+++ src/utils/typography.ts
@@ -1,17 +1,19 @@
 import Typography from 'typography';
 import Wordpress2016 from 'typography-theme-wordpress-2016';
 
-Wordpress2016.overrideThemeStyles = () => {
-  return {
-    'a.gatsby-resp-image-link': {
-      boxShadow: `none`,
-    },
-  };
+const wpTypo = {
+  ...Wordpress2016,
+  overrideThemeStyles() {
+    return {
+      'a.gatsby-resp-image-link': {
+        boxShadow: `none`,
+      },
+    };
+  },
+  googleFonts: [],
 };
 
-delete Wordpress2016.googleFonts;
-
-const typography = new Typography(Wordpress2016);
+const typography = new Typography(wpTypo);
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== `production`) {
