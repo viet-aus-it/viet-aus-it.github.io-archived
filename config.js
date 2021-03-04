39056dd32f11f571b1c5f903bb1c3860fbcd912f
Sam Huynh <samhwang2112.dev@gmail.com>
2021-03-04 23:39:44 +1100

    Add contentful config

--- config.js
+++ config.js
@@ -1,3 +1,23 @@
+const dotenv = require('dotenv');
+
+dotenv.config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
+const contentfulConfig = {
+  spaceId: process.env.CONTENTFUL_SPACE_ID,
+  accessToken:
+    process.env.CONTENTFUL_ACCESS_TOKEN ||
+    process.env.CONTENTFUL_DELIVERY_TOKEN,
+};
+
+const { spaceId, accessToken } = contentfulConfig;
+if (!spaceId || !accessToken) {
+  throw new Error(
+    'Contentful spaceId and the access token need to be provided.'
+  );
+}
+
 module.exports = {
   siteTitle: 'Viet-Aus-IT Blog',
   siteUrl: 'https://viet-aus-it.github.io',
@@ -23,4 +43,5 @@ module.exports = {
   socialLinks: {
     github: 'viet-aus-it',
   },
+  contentfulConfig,
 };
