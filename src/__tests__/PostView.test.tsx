1c617eda10379ff8cba66b4739652bb20a1b384e
Sam Huynh <samhwang2112.dev@gmail.com>
2021-03-05 10:51:25 +1100

    Update PostView test

--- src/__tests__/PostView.test.tsx
+++ src/__tests__/PostView.test.tsx
@@ -4,58 +4,20 @@ import PostView from '../components/Template/PostView';
 describe('Post View', () => {
   it('should render "proper" post nodes', () => {
     const fullNode = {
-      fields: {
-        slug: 'Test Post',
+      title: 'Test Post',
+      slug: 'Test Post',
+      publishDate: '2020-04-27T12:00+11:00',
+      description: {
+        description: 'Test Description',
       },
-      frontmatter: {
-        title: 'Test Post',
-        date: '27/04/2020',
-        description: '<p>This is just a test post</p>',
+      body: {
+        childMarkdownRemark: {
+          excerpt: '<p>This is just a test post</p>',
+        }
       },
-      except: '<p>This is just a test post</p>',
     };
 
     const tree = render(<PostView node={fullNode} />);
     expect(tree).toMatchSnapshot();
-  });
-
-  it('should render post nodes with missing frontmatter details', () => {
-    const withoutFrontmatterTitle = {
-      fields: {
-        slug: 'Test Post',
-      },
-      frontmatter: {
-        date: '27/04/2020',
-        description: '<p>This is just a test post</p>',
-      },
-      except: '<p>This is just a test post</p>',
-    };
-    const tree2 = render(<PostView node={withoutFrontmatterTitle} />);
-    expect(tree2).toMatchSnapshot();
-
-    const withoutFrontmatterDesc = {
-      fields: {
-        slug: 'Test Post',
-      },
-      frontmatter: {
-        title: 'Test Post',
-        date: '27/04/2020',
-      },
-      except: '<p>This is just a test post</p>',
-    };
-    const tree3 = render(<PostView node={withoutFrontmatterDesc} />);
-    expect(tree3).toMatchSnapshot();
-
-    const withoutFrontmatterTitleAndDesc = {
-      fields: {
-        slug: 'Test Post',
-      },
-      frontmatter: {
-        date: '27/04/2020',
-      },
-      except: '<p>This is just a test post</p>',
-    };
-    const tree4 = render(<PostView node={withoutFrontmatterTitleAndDesc} />);
-    expect(tree4).toMatchSnapshot();
   });
 });
